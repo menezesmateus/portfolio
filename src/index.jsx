a1457125fca91f93add018a0f6ff038463a067d3
menezesmateus <mateusgithub12@gmail.com>
2024-09-02 19:03:36 -0300

    finish

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import Cabecalho from "./pages/home/Cabecalho/Cabecalho.jsx"
+import Portifolio from "./pages/home/Portifolio/Portifolio.jsx"
+
+function index() {
+    return (
+    <>
+        <Cabecalho />
+        <Portifolio />
+    </>
+    )
+}
+
+export default index
